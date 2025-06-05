ddf13ed91edd443b6b28cc67804d8067929ac817
NEHA MS <pixelpulse236@gmail.com>
2025-06-05 10:54:31 +0530

    Delete backend/src/main/java/com/fileshare directory

--- backend/src/main/java/com/fileshare/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fileshare.repository;
-
-import com.fileshare.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
-}
